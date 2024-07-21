48645b52dc5bbf9079974bb83063fbe9e17ca48f
sathish091092 <m.sathish305@gmail.com>
2024-07-21 13:00:08 +0530

    test

---
+++ src/Demo/factorial.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class factorial {
+    public static void main(String[] args) {
+        int a=5;
+        int b =1;
+        for(int i=5; i>=1 ;i--){
+            b = b *i;
+        }
+        System.out.println(b);
+    }
+}
